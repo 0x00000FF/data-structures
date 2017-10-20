6720f111555cc93196ceabece81992745bc6651d
0x00000FF <doctor@kangnang.net>
2017-10-21 01:14:12 +0900

    STUCK

--- src/stack.hpp
+++ src/stack.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 #include <stdexcept>
 
@@ -6,57 +5,56 @@ template <typename T, int32_t _capacity>
 class Stack
 {
 public:
-    explicit      Stack   ();
-                  Stack   (Stack&  stack)                 = default;
-                  Stack   (Stack&& stack) noexcept        = default;
-                  ~Stack  () noexcept;
+    explicit         Stack   ();
+                     Stack   (Stack&  stack)                 = default;
+                     Stack   (Stack&& stack) noexcept        = default;
+                     ~Stack  () noexcept;
 
-    const bool    empty   () const noexcept;
-    const bool    full    () const noexcept;
-    const T&      peek    () const;
-    const T&&     pop     () noexcept;
+    const bool       empty   () const noexcept;
+    const bool       full    () const noexcept;
+    const T&         peek    () const;
+    const T&&        pop     () noexcept;
+    const int32_t    size    () const noexcept;
+    const T* const   ptr     () const noexcept;
 
-    void          push    (const T&& elem);
+    void             push    (const T&& elem);
 
-    //Stack&        operator= (Stack const&)                                    = default;
-    //const bool    operator==(const Stack<T, _capacity>&) noexcept             = default;
+    Stack<T, _capacity>&           operator= (Stack<T, _capacity> const&);
+    const bool                     operator==(const Stack<T, _capacity>&);
 
 private:
     int32_t   top  = -1;
-    
-protected:
     T*        data;
 };
 
 template <typename T, int32_t _capacity>
 Stack<T, _capacity>::Stack ()
 {
-    if (_capacity > 0)
-        data = new T[_capacity];
-    else
-        std::runtime_error("invalid capacity");
-};
+    static_assert(std::is_reference<T>::value, "reference type cannot be element of stack");
+
+    data = new T[_capacity];
+}
 
 template <typename T, int32_t _capacity>
 Stack<T, _capacity>::~Stack() noexcept
 {
     delete [] data;
-};
+}
 
 template <typename T, int32_t _capacity>
-const bool  Stack<T, _capacity>::empty() const noexcept
+const bool        Stack<T, _capacity>::empty() const noexcept
 {
     return top == -1;
-};
+}
 
 template <typename T, int32_t _capacity>
-const bool  Stack<T, _capacity>::full() const noexcept
+const bool        Stack<T, _capacity>::full() const noexcept
 {
     return top == _capacity - 1;
-};
+}
 
 template <typename T, int32_t _capacity>
-const T&    Stack<T, _capacity>::peek()  const
+const T&          Stack<T, _capacity>::peek()  const
 {
     if (empty())
         throw std::runtime_error("stack is empty");
@@ -65,20 +63,35 @@ const T&    Stack<T, _capacity>::peek()  const
 }
 
 template <typename T, int32_t _capacity>
-const T&&   Stack<T, _capacity>::pop()   noexcept
+const T&&         Stack<T, _capacity>::pop()   noexcept
 {
     if (empty())
         throw std::runtime_error("stack is empty");
 
-    return std::move(data[top--]);
-};
+    auto ret = std::move(data[top]);
+    data[top--].~T();
+
+    return ret;
+}
 
 template <typename T, int32_t _capacity>
-void        Stack<T, _capacity>::push(const T&& elem)
+void              Stack<T, _capacity>::push(const T&& elem)
 {
     if (full())
         throw std::runtime_error("stack is full");
 
     top++;
     data[top] = elem;
-};
+}
+
+template <typename T, int32_t _capacity>
+const int32_t     Stack<T, _capacity>::size() const noexcept
+{
+    return _capacity;
+}
+
+template <typename T, int32_t _capacity>
+const T* const    Stack<T, _capacity>::ptr() const noexcept
+{
+    return data;
+}
